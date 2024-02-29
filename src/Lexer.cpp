db85288e28c8a432d5c86a8fcfbbe9de5ba7f2ee
spineda2019 <spineda@wpi.edu>
2024-02-29 17:01:25 -0500

    process input file name and stub out lexing

--- src/Lexer.cpp
+++ src/Lexer.cpp
@@ -1,1 +1,25 @@
 // Copyright 2024 Sebastian Pineda
+#include <filesystem>
+#include <string>
+#include <vector>
+
+#include "Lexer.hpp"
+#include "Token.hpp"
+
+namespace Lexer {
+int ProcessFileName(const std::string &file_name) {
+  if (file_name != "main.he") {
+    return -1;
+  } else if (!std::filesystem::exists(file_name)) {
+    return -2;
+  } else {
+    return 0;
+  }
+}
+
+std::vector<Token> LexMainFile(const std::string &file_name) {
+  std::vector<Token> tokens{};
+
+  return std::move(tokens);
+}
+} // namespace Lexer
--- src/include/Lexer.hpp
+++ src/include/Lexer.hpp
@@ -2,10 +2,14 @@
 #ifndef SRC_INCLUDE_LEXER_HPP_
 #define SRC_INCLUDE_LEXER_HPP_
 
-#include "Token.hpp"
+#include <string>
 #include <vector>
+
+#include "Token.hpp"
+
 namespace Lexer {
-std::vector<Token> &&LexMainFile();
+int ProcessFileName(const std::string &file_name);
+std::vector<Token> LexMainFile(const std::string &file_name);
 } // namespace Lexer
 
 #endif // SRC_INCLUDE_LEXER_HPP_
