bfb8c5e8362fc922c7db9fbeb6a640a8ce662c55
spineda2019 <spineda@wpi.edu>
2024-03-01 16:58:42 -0500

    classify current char, helper function, constexpr arrays to hold operators

--- src/Lexer.cpp
+++ src/Lexer.cpp
@@ -1,10 +1,15 @@
 // Copyright 2024 Sebastian Pineda
+#include <algorithm>
+#include <cctype>
 #include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <optional>
 #include <string>
 #include <vector>
 
-#include "Lexer.hpp"
-#include "Token.hpp"
+#include "include/Lexer.hpp"
+#include "include/Token.hpp"
 
 namespace Lexer {
 int ProcessFileName(const std::string &file_name) {
@@ -17,8 +22,49 @@ int ProcessFileName(const std::string &file_name) {
   }
 }
 
-std::vector<Token> LexMainFile(const std::string &file_name) {
+const std::optional<CharacterType> ClassifyCharacter(const char &letter) {
+  //
+  if (std::isdigit(letter)) {
+    return CharacterType::NumberLiteral;
+  } else if (letter == '.') {
+    return CharacterType::Decimal;
+  } else if (letter == '\n') {
+    return CharacterType::NewLine;
+  } else if (std::isspace(letter)) {
+    return CharacterType::Whitespace;
+  } else if (std::find(valid_simple_operators.begin(),
+                       valid_simple_operators.end(),
+                       letter) != valid_simple_operators.end()) {
+    return CharacterType::Operator;
+  } else if (letter == '(' || letter == ')') {
+    return CharacterType::GroupingSymbol;
+  } else if (letter == ';') {
+    return CharacterType::SemiColon;
+  } else if (std::isalpha(letter)) {
+    return CharacterType::Letter;
+  } else {
+    return {};
+  }
+}
+
+std::optional<std::vector<Token>> LexMainFile(const std::string &file_name) {
   std::vector<Token> tokens{};
+  std::ifstream main_file(file_name);
+
+  char current_character{};
+  std::string built_lexeme{};
+  std::optional<CharacterType> current_character_type(
+      CharacterType::Whitespace);
+  LexemeType current_lexeme_type(LexemeType::Empty);
+
+  while (main_file.get(current_character)) {
+    std::cout << "found a char: " << current_character << std::endl;
+    current_character_type = ClassifyCharacter(current_character);
+    if (!current_character_type.has_value()) {
+      // Terminate Compilation
+      return {};
+    }
+  }
 
   return std::move(tokens);
 }
--- src/include/Lexer.hpp
+++ src/include/Lexer.hpp
@@ -2,14 +2,47 @@
 #ifndef SRC_INCLUDE_LEXER_HPP_
 #define SRC_INCLUDE_LEXER_HPP_
 
+#include <array>
+#include <cstdint>
+#include <optional>
 #include <string>
 #include <vector>
 
 #include "Token.hpp"
 
 namespace Lexer {
+
+constexpr std::array<char, 6> valid_simple_operators{'+', '-', '/',
+                                                     '*', '<', '>'};
+
+enum class CharacterType : std::uint8_t {
+  NumberLiteral,
+  Decimal,
+  NewLine,
+  Whitespace,
+  Operator,
+  GroupingSymbol,
+  SemiColon,
+  Letter,
+};
+
+enum class LexemeType : std::uint8_t {
+  SimpleOperator,
+  CompoundOperator,
+  LeftParenthesis,
+  RightParenthesis,
+  LeftBracket,
+  RightBracket,
+  IntegerLiteral,
+  FloatLiteral,
+  Identifier,
+  Empty
+};
+
+const std::optional<CharacterType> ClassifyCharacter(const char &letter);
+
 int ProcessFileName(const std::string &file_name);
-std::vector<Token> LexMainFile(const std::string &file_name);
+std::optional<std::vector<Token>> LexMainFile(const std::string &file_name);
 } // namespace Lexer
 
 #endif // SRC_INCLUDE_LEXER_HPP_
